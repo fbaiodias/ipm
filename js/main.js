c119cf907eef751d813127d76803bcda92fa5e45
Francisco Baio Dias <xicombd@gmail.com>
2014-04-07 12:42:26 +0100

    kindof menu

--- js/main.js
+++ js/main.js
@@ -1,71 +1,31 @@
-// create an new instance of a pixi stage
-var stage = new PIXI.Stage(0x66FF99);
-
-// create a renderer instance.
-var renderer = PIXI.autoDetectRenderer(400, 300);
-
-// add the renderer view element to the DOM
-document.body.appendChild(renderer.view);
-
-requestAnimFrame( animate );
-
-// create a texture from an image path
-var texture = PIXI.Texture.fromImage("img/bunny.png");
-// create a new Sprite using the texture
-var bunny = new PIXI.Sprite(texture);
-
-// center the sprites anchor point
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
-
-// move the sprite t the center of the screen
-bunny.position.x = 200;
-bunny.position.y = 150;
-
-stage.addChild(bunny);
-
-
-function init() {
-  setEventHandlers();
-  renderer.resize(window.innerWidth, window.innerHeight);
-}
-
-function animate() {
-
-  requestAnimFrame( animate );
-
-  // just for fun, lets rotate mr rabbit a little
-  bunny.rotation += 0.1;
-
-  // render the stage
-  renderer.render(stage);
-}
-
-// Event handlers!!
-var setEventHandlers = function() {
-	// Keyboard
-	window.addEventListener("keydown", onKeydown, false);
-	window.addEventListener("keyup", onKeyup, false);
-
-	// Window resize
-	window.addEventListener("resize", onResize, false);
-};
-
-// Keyboard key down
-function onKeydown(e) {
-	keys.onKeyDown(e);
-};
-
-// Keyboard key up
-function onKeyup(e) {
-	keys.onKeyUp(e);
-};
-
-// Browser window resize
-function onResize(e) {
-	// Maximise the canvas
-	renderer.resize(window.innerWidth, window.innerHeight);
-};
-
-
-init();
+$(window).load(function(){
+  var li = $('li');
+  var liSelected;
+  $(window).keydown(function(e){
+      if(e.which === 40){
+          if(liSelected){
+              liSelected.removeClass('selected');
+              next = liSelected.next();
+              if(next.length > 0){
+                  liSelected = next.addClass('selected');
+              }else{
+                  liSelected = li.eq(0).addClass('selected');
+              }
+          }else{
+              liSelected = li.eq(0).addClass('selected');
+          }
+      }else if(e.which === 38){
+          if(liSelected){
+              liSelected.removeClass('selected');
+              next = liSelected.prev();
+              if(next.length > 0){
+                  liSelected = next.addClass('selected');
+              }else{
+                  liSelected = li.last().addClass('selected');
+              }
+          }else{
+              liSelected = li.last().addClass('selected');
+          }
+      }
+  });
+});

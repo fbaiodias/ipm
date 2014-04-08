2de497a6e7ce0323a13e99dbf2665904919c0ef8
Francisco Baio Dias <xicombd@gmail.com>
2014-04-08 23:13:00 +0100

    menus

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
+var li, liSelected;
+
 $(window).load(function(){
-  var li = $('li');
-  var liSelected;
+  goToMenu("menu");
+
+
+  li = $('li');
+  liSelected = li.eq(0).addClass('selected');;;
   $(window).keydown(function(e){
       if(e.which === 40){
           if(liSelected){
@@ -26,6 +31,43 @@ $(window).load(function(){
           }else{
               liSelected = li.last().addClass('selected');
           }
+      }else if(e.which === 39){
+        if(liSelected && liSelected[0].title) {
+          goToMenu(liSelected[0].title);
+        } else {
+          if(liSelected[0].children[0].checked) {
+            liSelected[0].children[0].checked = false;
+          } else {
+            liSelected[0].children[0].checked = true;
+          }
+        }
+      }else if(e.which === 37){
+        var ul = $('ul');
+        if(ul && ul[0].title) {
+          goToMenu(ul[0].title);
+        }
       }
+
   });
 });
+
+function goToMenu(id) {
+  var html = getHTML("partials/"+id+".html");
+
+  $('#container').html(html);
+
+  li = $('li');
+  liSelected = li.eq(0).addClass('selected');
+}
+
+function getHTML(url) {
+  var data;
+  $.ajax({
+    async: false,
+    url: url,
+    success: function(response) {
+      data = response;
+    }
+  });
+  return data;
+}

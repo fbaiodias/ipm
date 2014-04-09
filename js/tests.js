a95d8e65ec4d61bad52d2fab0d8a1b6df418ac33
Raicuparta <raicuparta@gmail.com>
2014-04-09 20:04:32 +0100

    sexy timer

--- js/tests.js
+++ js/tests.js
@@ -13,54 +13,68 @@ var options = {
         "minutes": 00,
       }
     },
-    "type": []
+    "type": [],
+    "start": false,
+    "time": 30
   } 
 } 
 
 $(window).load(function(){
   goToMenu("testes-calculo");
 
-
   li = $('b');
   liSelected = li.eq(0).addClass('selected');;;
   $(window).keydown(function(e){
 
-      if(e.which === 39){
-          if(liSelected){
-              liSelected.removeClass('selected');
-              next = liSelected.next();
-              if(next.length > 0){
-                  liSelected = next.addClass('selected');
-              }else{
-                  liSelected = li.eq(0).addClass('selected');
-              }
-          }else{
-              liSelected = li.eq(0).addClass('selected');
-          }
-      }else if(e.which === 37){
-          if(liSelected){
-              liSelected.removeClass('selected');
-              next = liSelected.prev();
-              if(next.length > 0){
-                  liSelected = next.addClass('selected');
-              }else{
-                  liSelected = li.last().addClass('selected');
-              }
-          }else{
-              liSelected = li.last().addClass('selected');
-          }
-      }else if(e.which === 38){
-        var number = eval(liSelected[0].textContent);
-        if(number<9) {number++;}
-        else {number = 0}
-        liSelected[0].textContent = number;          
-      }else if(e.which === 40){
-        var number = eval(liSelected[0].textContent);
-        if(number>0) {number--;}
-        else {number = 9}
-        liSelected[0].textContent = number;          
+      if(options.tests.start) {
+        if(e.which === 39){
+            if(liSelected){
+                liSelected.removeClass('selected');
+                next = liSelected.next();
+                if(next.length > 0){
+                    liSelected = next.addClass('selected');
+                }else{
+                    liSelected = li.eq(0).addClass('selected');
+                }
+            }else{
+                liSelected = li.eq(0).addClass('selected');
+            }
+        }else if(e.which === 37){
+            if(liSelected){
+                liSelected.removeClass('selected');
+                next = liSelected.prev();
+                if(next.length > 0){
+                    liSelected = next.addClass('selected');
+                }else{
+                    liSelected = li.last().addClass('selected');
+                }
+            }else{
+                liSelected = li.last().addClass('selected');
+            }
+        }else if(e.which === 38){
+          var number = eval(liSelected[0].textContent);
+          if(number<9) {number++;}
+          else {number = 0}
+          liSelected[0].textContent = number;          
+        }else if(e.which === 40){
+          var number = eval(liSelected[0].textContent);
+          if(number>0) {number--;}
+          else {number = 9}
+          liSelected[0].textContent = number;          
+        }
+      } else {
+        options.tests.start = true;
+        $("#problemBefore").toggle();
+        $("#problemAfter").toggle();
+        $("#timer").toggle();
+
+        setTimeout(testCountDown, 1000);
+
+
       }
 
+
+      
   });
 });
 
@@ -83,4 +97,16 @@ function getHTML(url) {
     }
   });
   return data;
+}
+
+function testCountDown() {
+  if (options.tests.time <= 0) {
+    $("#problemAfter").toggle();
+    $("#testFailed").toggle();
+    $("#timer").toggle();
+  } else {
+    options.tests.time--;
+    $('#timerNumber').text(options.tests.time);
+    setTimeout(testCountDown, 1000);
+  }
 }
